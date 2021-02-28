5e949f860796bc01f5acdcae2ca4e0b74fc23ab7
Marios Chr <marios-christou@hotmail.com>
2021-02-28 16:29:19 +0200

    Commit

---
+++ Horkuin/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AquariusMax.Medieval;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public static bool GameIsPaused = false;
+    public DemoCharacter characterScript;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (GameIsPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        pauseMenuUI.SetActive(false);
+        characterScript.enabled = true;
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause() {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        pauseMenuUI.SetActive(true);
+        characterScript.enabled = false;
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
