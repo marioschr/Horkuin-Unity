12eac5dba9386afcf59c29bebf29fd9fc81ebe94
Marios Chr <marios-christou@hotmail.com>
2021-03-08 01:47:21 +0200

    commit

---
+++ Horkuin/Assets/Modified Animals/Goats/HorsesMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+public class HorsesMovement : MonoBehaviour
+{
+    public GameObject[] positions;
+    private Transform transform;
+    private NavMeshAgent navMeshAgent;
+    private Animator animator;
+    private Vector3 s;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Transform>().transform.position = positions[Random.Range(0, positions.Length)].transform.position;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        transform = positions[Random.Range(0, positions.Length)].transform;
+        navMeshAgent.SetDestination(transform.position);
+    }
+    
+    void Update()
+    {
+        if (navMeshAgent.remainingDistance < 0.8f)
+        {
+            if (Random.Range(0f, 1f) < 0.2f)
+            {
+                animator.SetBool("Eating", true);
+                navMeshAgent.speed = 0;
+                Invoke("StopEating",3f);
+            }
+            else if (animator.GetBool("Eating") == false)
+            {
+                navMeshAgent.speed = Random.Range(0.95f, 1.1f);
+                transform = positions[Random.Range(0, positions.Length)].transform;
+                navMeshAgent.SetDestination(transform.position);
+            }
+        } else if (navMeshAgent.remainingDistance >= 0.8f && navMeshAgent.speed == 0) {
+            navMeshAgent.speed = Random.Range(0.95f, 1.1f);
+        }
+        else {
+            s = navMeshAgent.transform.InverseTransformDirection(navMeshAgent.velocity).normalized;
+            float speed = s.z;
+            float turn = s.x;
+            animator.SetFloat("Forward",speed );
+            animator.SetFloat("LeftRight", turn);
+            //animator.SetFloat("Move Speed", navMeshAgent.velocity.);
+        }
+    }
+
+    void StopEating(){
+        animator.SetBool("Eating", false);
+    }
+}
---
+++ Horkuin/Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    public CinemachineVirtualCamera firstPerson;
+    public GameObject character, head;
+
+    private bool thirdP = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (thirdP)
+            {
+                firstPerson.Priority = 21;
+	            Invoke("DisableBody", 0.35f);
+                thirdP = false;
+		        head.SetActive(true);
+            }
+            else
+            {
+                firstPerson.Priority = 19;
+                thirdP = true;
+		        character.SetActive(true);
+            }
+        }
+    }
+
+    void DisableBody() {
+	     character.SetActive(false);
+    }
+}
